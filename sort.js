d25f716710df5fae94a4d99573d7885096fcf5f5
Yutaro Nakajima <yutaro.nakajima@secure-i.jp>
2024-06-15 16:26:38 +0900

    first commit

--- sort.js
+++ sort.js
@@ -1,2 +1,11 @@
+var sortnumber = function (number) {
+    number.sort(function (a, b) {
+        if (a == b) {
+            return 0;
+        }
+        return a < b ? -1 : 1;
+    });
+};
 var number = [19, 3, 81, 1, 24, 21];
+sortnumber(number);
 console.log(number);
